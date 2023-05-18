6d74c1381d1cb3cd96aacfc34a1899cf64e66a67
bwrosen20 <bwrosen20@gmail.com>
2023-05-18 18:39:39 -0400

    commit tree c9f1b38ffbc777a980c8436147f145e277c09196

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    name_array=[]
+    for name in names do 
+        name_array << "Hello, my name is #{name}."
+    end
+    name_array
+end
+
+def assign_rooms(names)
+    room_array=*(1..7)
+    assign_array=[]
+    for name in names do
+        assign_array << "Hello, #{name}! You'll be assigned to room #{room_array.shift}!"
+    end
+    assign_array
+end
+
+def printer(names)
+    badges=batch_badge_creator(names)
+    rooms=assign_rooms(names)
+    i=0
+    j=0
+    for name in names do
+        puts badges[i]
+        i+=1
+    end 
+    
+    for name in names do
+        puts rooms[j]
+        j+=1
+    end
+
+
+end
